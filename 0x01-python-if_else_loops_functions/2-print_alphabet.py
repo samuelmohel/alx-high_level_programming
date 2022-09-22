90da5f3116fb44187a17f25f9f99d2cefe8b85e6
samuelmohel <samuelmohel@gmail.com>
2022-09-21 18:21:50 -0700

    updated

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for i in 'abcdefghijklmnopqrstuvwxyz':
-    if (i != "q" and i != "e"):
-        print("{}".format(i), end="")
+for i in range(ord('a'), ord('z') + 1):
+    print("{:s}".format(chr(i)), end="")
