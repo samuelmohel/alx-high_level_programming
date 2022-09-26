cd2abf4fa6424a0a6601c54ec5c85b229e3f32d5
samuelmohel <samuelmohel@gmail.com>
2022-09-26 13:02:15 -0700

    first

---
+++ 0x02-python-import_modules/add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: seconf integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
